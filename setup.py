4676ee3c910f13f14230a48451e80a7a00dff2bc
Pantapan1 <120321842+Pantapan1@users.noreply.github.com>
2026-05-08 10:29:22 +0700

    Bump version from 0.4.0 to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rpython-lang',
-    version='0.4.0',
+    version='0.4.1',
     description='Русский Python — пиши код на русском языке',
     py_modules=['transpiler'],
     entry_points={
